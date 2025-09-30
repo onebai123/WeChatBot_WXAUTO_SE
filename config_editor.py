aea7860d69dcb93565a8ad2eddb68ea8b917dd0b
liang <2424123787@qq.com>
2025-09-30 18:36:53 +0800

    修复远程访问问题

--- config_editor.py
+++ config_editor.py
@@ -3762,5 +3762,5 @@ if __name__ == '__main__':
     
     Timer(1, open_browser).start()  # 延迟1秒确保服务器已启动
     
-    app.run(host="0.0.0.0", debug=False, port=PORT)
+    app.run(host="127.0.0.1", debug=False, port=PORT)
     
